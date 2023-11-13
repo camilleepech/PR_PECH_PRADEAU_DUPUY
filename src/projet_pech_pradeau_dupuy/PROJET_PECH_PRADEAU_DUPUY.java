a141bd19fac9512e2a773b38124e263077aaaf2d
Camille PECH <camille.pech@epfedu.fr>
2023-11-13 14:58:59 +0100

    création dépot

---
+++ src/projet_pech_pradeau_dupuy/PROJET_PECH_PRADEAU_DUPUY.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet_pech_pradeau_dupuy;
+
+/**
+ *
+ * @author Camille
+ */
+public class PROJET_PECH_PRADEAU_DUPUY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("PROJET TEAM");
+    }
+    
+}
